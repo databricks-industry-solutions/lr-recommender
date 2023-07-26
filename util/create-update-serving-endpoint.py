a707299b2cd6b8d138220ffcbe746ee2769b9d95
dbbnicole <dbbnicole>
2023-07-26 20:06:32 +0000

    Fix create update serving endpoint functions

--- util/create-update-serving-endpoint.py
+++ util/create-update-serving-endpoint.py
@@ -12,7 +12,7 @@ creds = get_databricks_host_creds()
 
 def endpoint_exists(serving_endpoint_name):
   """Check if an endpoint with the serving_endpoint_name exists"""
-  url = f"https://{serving_host}/api/2.0/serving-endpoints/{serving_endpoint_name}"
+  url = f"{serving_host}/api/2.0/serving-endpoints/{serving_endpoint_name}"
   headers = { 'Authorization': f'Bearer {creds.token}' }
   response = requests.get(url, headers=headers)
   return response.status_code == 200
@@ -20,7 +20,7 @@ def endpoint_exists(serving_endpoint_name):
 def wait_for_endpoint(serving_endpoint_name):
   """Wait until deployment is ready, then return endpoint config"""
   headers = { 'Authorization': f'Bearer {creds.token}' }
-  endpoint_url = f"https://{serving_host}/api/2.0/serving-endpoints/{serving_endpoint_name}"
+  endpoint_url = f"{serving_host}/api/2.0/serving-endpoints/{serving_endpoint_name}"
   response = requests.request(method='GET', headers=headers, url=endpoint_url)
   while response.json()["state"]["ready"] == "NOT_READY" or response.json()["state"]["config_update"] == "IN_PROGRESS" : # if the endpoint isn't ready, or undergoing config update
     print("Waiting 30s for deployment or update to finish")
@@ -32,7 +32,7 @@ def wait_for_endpoint(serving_endpoint_name):
 def create_endpoint(serving_endpoint_name, served_models):
   """Create serving endpoint and wait for it to be ready"""
   print(f"Creating new serving endpoint: {serving_endpoint_name}")
-  endpoint_url = f'https://{serving_host}/api/2.0/serving-endpoints'
+  endpoint_url = f'{serving_host}/api/2.0/serving-endpoints'
   headers = { 'Authorization': f'Bearer {creds.token}' }
   request_data = {"name": serving_endpoint_name, "config": {"served_models": served_models}}
   json_bytes = json.dumps(request_data).encode('utf-8')
@@ -44,7 +44,7 @@ def create_endpoint(serving_endpoint_name, served_models):
 def update_endpoint(serving_endpoint_name, served_models):
   """Update serving endpoint and wait for it to be ready"""
   print(f"Updating existing serving endpoint: {serving_endpoint_name}")
-  endpoint_url = f"https://{serving_host}/api/2.0/serving-endpoints/{serving_endpoint_name}/config"
+  endpoint_url = f"{serving_host}/api/2.0/serving-endpoints/{serving_endpoint_name}/config"
   headers = { 'Authorization': f'Bearer {creds.token}' }
   request_data = { "served_models": served_models, "traffic_config": traffic_config }
   json_bytes = json.dumps(request_data).encode('utf-8')
